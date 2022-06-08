fb3f01bb738eacdf171145c918bdc816d862330f
taisbelucik <taisbelucik@hotmail.com>
2022-06-08 13:57:19 -0300

    Limpar campos após adicionar as informações de cadastro

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-const { listen } = require("express/lib/application")
+const nodemon = require("nodemon")
 
 
 function cadastrar(){
@@ -11,6 +11,8 @@ var caract = document.getElementById('feature').value
 var raca = document.getElementById('breed').value
 var qtdLinhas = tb.rows.length
 var linha = tb.insertRow(qtdLinhas)
+
+
 
 var cellCod = linha.insertCell(0)
 var cellNome = linha.insertCell(1)
@@ -27,24 +29,43 @@ cellIdade.innerHTML = idade
 cellPort.innerHTML = porte
 cellCarc.innerHTML = caract
 cellRaca.innerHTML = raca
-cellAcao.innerHTML = '<div class="cont-botao"><button class="botao bte" onclick="excluir(this)">Excluir</button><button class="btd botao">Editar</button></div>'
+cellAcao.innerHTML = '<div class="cont-botao"><button class="botao bte" onclick="excluir(this)">Excluir</button><button class="btd botao" onclick="editar(this)">Editar</button></div>'
+
+document.getElementById('name').value = ''
+document.getElementById('age').value = ''
+document.getElementById('size').value = ''
+document.getElementById('feature').value = ''
+document.getElementById('breed').value = ''
+
 
 }
 
 
-
+function bgLinhaSelecionada(row){  //função que deixa a linha selecionada
+    row.parentNode.parentNode.parentNode.style.backgroundColor = "#bdbdbd"  //pega a row, acessa os parentes até chegar no td, e seta o background-color para a cor cinza
+}
 
 function excluir(row){
-       console.log(row.parentNode.parentNode.parentNode)
+       console.log(row.parentNode.parentNode.parentNode)    
        row.parentNode.parentNode.parentNode.remove()
-
-//     console.log('teste')
-// tb.deleteCell(0)
-// tb.deleteCell(1)
-// tb.deleteCell(2)
-// tb.deleteCell(3)
-// tb.deleteCell(4)
-// tb.deleteCell(5)
-// tb.deleteCell(6)
 }
+
+function editar(row){
+    const celulas = row.parentNode.parentNode.parentNode.children
+
+    bgLinhaSelecionada(row) //aqui é chamado a função que muda a cor da linha ao ser selecionada, mas é necessário passar pelo paramtro a linha
+
+    getElementById('bt-att')
+
+    console.log(row.parentNode.parentNode.parentNode)
+    
+    // celulas[1].innerText = document.getElementById('name').value
+    
+    // for (const celula of celulas) {
+        
+    //     console.log(celula.innerText)
+    // }
+    }
+
+
 
